be23c8e0482d478700e4d2c8de3f3b031a80b18c
JusticeShurup <69842449+JusticeShurup@users.noreply.github.com>
2020-10-22 18:02:27 +0500

    Add files via upload

---
+++ Visitors/StudentVisitor.h
@@ -0,0 +1,10 @@
+#ifndef _STUDENTVISITOR_H_
+#define _STUDENTVISITOR_H_
+#include "../student.h"
+class StudentVisitor{ 
+    public:
+    virtual void StartVisit() = 0;
+    virtual void VisitStudent(int number, Student *student) = 0;
+    virtual void finishVisit() = 0;
+};
+#endif
